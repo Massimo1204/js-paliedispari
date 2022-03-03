f0695c328c7acf4a258817b4a26b6a1115edc79b
Massimo1204 <xiangmassimo@gmail.com>
2022-03-03 18:07:17 +0100

    palindrome update

--- js/main.js
+++ js/main.js
@@ -1,35 +1,57 @@
-const userInput = document.getElementById('user-input');
-const result = document.getElementById('result');
+const userInputPalindrome = document.getElementById('user-input-palindrome');
+const resultPalindrome = document.getElementById('result-palindrome');
+const evenButton = document.getElementById('pari');
+const oddButton = document.getElementById('dispari');
+const oddEvenInput = document.getElementById('odd-even-input');
+const oddEvenResult = document.getElementById('game-result');
 
 function keyEnter(e){
-    if(e.keyCode == '13'){
-        if(checkPalindrome(userInput.value)==true){
-            result.classList.remove('text-danger');
-            result.classList.add('text-success');
-            result.innerHTML=' la parola è palindroma';
-        }
-        else{
-            result.classList.remove('text-success');
-            result.classList.add('text-danger');
-            result.innerHTML=' la parola non è palindroma';
-        }
+    if(e.keyCode == '13'){  
+        displayPalindromeResult(checkPalindrome(userInputPalindrome.value.toLowerCase()));
+    }
+}
+
+function displayPalindromeResult(check){
+    if(check==true){
+        resultPalindrome.classList.remove('text-danger');
+        resultPalindrome.classList.add('text-success');
+        resultPalindrome.innerHTML=' la parola è palindroma';
+    }
+    else if (check == false){
+        resultPalindrome.classList.remove('text-success');
+        resultPalindrome.classList.add('text-danger');
+        resultPalindrome.innerHTML=' la parola non è palindroma';
+    }
+    else if (check == 'vuoto'){
+        resultPalindrome.classList.remove('text-success');
+        resultPalindrome.classList.remove('text-danger');
+        resultPalindrome.innerHTML = 'inserisci una parola';
     }
 }
 
 function checkPalindrome(word){
     let palindrome=0;
-    for(let i=0; i<word.length ;i++){
-        console.log(word.charAt(i));
-        console.log(word.charAt(word.length-i-1));
-        if(word.charAt(i) != word.charAt(word.length-i-1)){
-            palindrome += 1;
-        }
-        console.log(palindrome);
+    if(word.length==0){
+        return 'vuoto';
     }
-    if(palindrome==0){
-        return true;
-    }else{
-        return false;
+    else{
+        for(let i=0; i<word.length ;i++){
+            console.log(word.charAt(i));
+            console.log(word.charAt(word.length-i-1));
+            if(word.charAt(i) != word.charAt(word.length-i-1)){
+                palindrome += 1;
+            }
+        }
+        if(palindrome==0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
-userInput.addEventListener('keypress', keyEnter);
+
+function playOddEven(){}
+
+userInputPalindrome.addEventListener('keypress', keyEnter);
+evenButton.addEventListener('click',playOddEven);
+oddEvenInput.addEventListener('keypress', keyEnter);
