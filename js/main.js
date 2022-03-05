a8d7c66ad51ee20f47ab011bb70d8e98c7552dcb
Massimo1204 <xiangmassimo@gmail.com>
2022-03-05 23:40:58 +0100

    update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,15 @@ const evenButton = document.getElementById('pari');
 const oddButton = document.getElementById('dispari');
 const oddEvenInput = document.getElementById('odd-even-input');
 const oddEvenResult = document.getElementById('game-result');
+const oddEvenInputContainer = document.getElementById('odd-even-input-container');
+const oddEvenChoice = document.getElementById('choice');
 let oddOrEven;
+
+function keyEnterPalindrome(e) {
+    if(e.key == 'Enter'){  
+        displayPalindromeResult(checkPalindrome(userInputPalindrome.value.toLowerCase()));
+    }
+}
 
 function displayPalindromeResult(check){
     if(check==true){
@@ -45,53 +53,75 @@ function checkPalindrome(word){
     }
 }
 
-function randomNumberGenerator(){
-    return (Math.floor(Math.random()*4)+1);
+function randomNumberGenerator(spectrum,startingPoint){
+    return (Math.floor(Math.random()*spectrum)+startingPoint);
 }
 for(let i=0;i<100;i++){
     console.log(randomNumberGenerator());
 
 }
 function playOdd(){
+    oddEvenResult.innerHTML='';
+    oddEvenInputContainer.classList.remove('d-none');
+    oddEvenChoice.innerHTML = "Hai scelto <span class='text-warning fw-bold'> Dispari</span>";
     oddOrEven = 1;
     return oddOrEven;
 }
 function playEven(){
+    oddEvenResult.innerHTML='';
+    oddEvenInputContainer.classList.remove('d-none');
+    oddEvenChoice.innerHTML = "Hai scelto <span class='text-danger fw-bold'> Pari</span>";
     oddOrEven = 0;
     return oddOrEven;
 }
 
-userInputPalindrome.addEventListener('keypress', function(e) {
-    if(e.key == 'Enter'){  
-        displayPalindromeResult(checkPalindrome(userInputPalindrome.value.toLowerCase()));
+function displayNumbersPlayed(sum, randNum,userInput){
+    sum = randNum+userInput;
+    oddEvenResult.innerHTML+= "Hai giocato <span class='text-primary fw-bold'>"  + userInput +"</span><br> Il computer gioca<span class='text-danger fw-bold'> "+randNum+  "</span> <br> Somma = <span class='text-success fw-bold'>"+sum+"</span><br>";
+}
+
+function checkGameResult(oddOrEven , userInput){
+    let sum=0;
+    let randNum=randomNumberGenerator(5,1);
+
+    if(isNaN(userInput) || userInput==''){
+        oddEvenResult.innerHTML="<span class='text-danger'>Attenzione!</span> <br> non hai inserito un numero.";
     }
-});
+    else if (userInput > 5 || userInput<1){
+        oddEvenResult.innerHTML="<span class='text-danger'>Attenzione!<br></span> inserisci un numero compreso tra 1 e 5.";
+    }
+    else if(oddOrEven==0){
+        displayNumbersPlayed(sum,randNum,userInput);
+        if(sum%2==0){
+            oddEvenResult.innerHTML+=" <span class='text-primary  fw-bold my-text-size'> hai vinto!</span> ";
+        }
+        else {
+            oddEvenResult.innerHTML+=" <span class='text-danger fw-bold my-text-size> hai perso!</span> ";
+        }
+    }
+    else if(oddOrEven==1){
+        displayNumbersPlayed(sum,randNum,userInput);
+        if(sum%2==0){
+            oddEvenResult.innerHTML+=" <span class='text-danger  fw-bold my-text-size'> hai perso!</span> ";
+        }
+        else {
+            oddEvenResult.innerHTML+=" <span class='text-primary fw-bold my-text-size'> hai vinto!</span> ";
+        }
+    }
+}
+
+function keyEnterOddEven(e){
+    if(e.key == 'Enter'){  
+        oddEvenInputContainer.classList.add('d-none');
+        checkGameResult(oddOrEven , parseInt(oddEvenInput.value));
+        oddEvenInput.value='';
+    }
+}
+
+
+userInputPalindrome.addEventListener('keypress', keyEnterPalindrome);
 
 oddButton.addEventListener('click',playOdd);
 evenButton.addEventListener('click',playEven);
 
-oddEvenInput.addEventListener('keypress', function(e){
-    if(e.key == 'Enter'){  
-        let sum=0;
-        if(oddOrEven==0){
-            sum =parseInt(randomNumberGenerator())+parseInt(oddEvenInput.value);
-            oddEvenResult.innerHTML=randomNumberGenerator()+' + '+ oddEvenInput.value +' = '+sum;
-            if(sum%2==0){
-                oddEvenResult.innerHTML+='  hai vinto ';
-            }
-            else {
-                oddEvenResult.innerHTML+='  hai perso ';
-            }
-        }
-        if(oddOrEven==1){
-            sum =parseInt(randomNumberGenerator())+parseInt(oddEvenInput.value);
-            oddEvenResult.innerHTML=randomNumberGenerator()+' + '+ oddEvenInput.value +' = '+sum;
-            if(sum%2==0){
-                oddEvenResult.innerHTML+='  hai perso ';
-            }
-            else {
-                oddEvenResult.innerHTML+='  hai vinto ';
-            }
-        }
-    }
-});
+oddEvenInput.addEventListener('keypress', keyEnterOddEven);
